2d3020a9e8ac92a2ad555fdcc9d30fbfc6479596
hadarean <Ioan-Dumitru.Hadarean@arrk-engineering.com>
2023-03-26 12:52:04 +0300

    Added Thread object example

--- views/mainview.py
+++ views/mainview.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-from PyQt6.QtCore import QThreadPool, QThread
+from PyQt6.QtCore import QThreadPool, QThread, QObject, pyqtBoundSignal, pyqtSignal
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMdiArea, QVBoxLayout, QWidget
 
 from models.mainmodel import MainModel
@@ -28,10 +28,20 @@ class MainView(object):
         self._main_widget = QWidget()
         self._main_widget.setProperty("class", "main-window-widget")
         self._main_widget.setLayout(self._main_layout)
-        self.pool = QThreadPool()
-        self.extractor = ExtractRestaurantsByCity(self.extract_data)
+        task = ExtractDataTask([1, 2, 3, 4])
+        self.task = task
+        self.extractor = QThread()
+        task.moveToThread(self.extractor)
+        self.extractor.started.connect(task.start_extraction)
+        task.extractDataFinishedSignal.connect(self.extractor.quit)
+        task.extractDataFinishedSignal.connect(task.deleteLater)
+        task.extractDataFinishedSignal.connect(self.show_success_message)
+        self.extractor.finished.connect(self.thread_done)
+        self.extractor.finished.connect(self.extractor.deleteLater)
         self.extractor.start()
-        self.extractor.finished.connect(self.thread_finished)
+
+    def thread_done(self):
+        print('Done')
 
     def extract_data(self):
         for i in range(10):
@@ -43,6 +53,10 @@ class MainView(object):
     def run(self):
         return self._app.exec()
 
+    def show_success_message(self, message):
+        print(message)
+        print()
+
 
 class ExtractRestaurantsByCity(QThread):
 
@@ -52,3 +66,19 @@ class ExtractRestaurantsByCity(QThread):
 
     def run(self):
         self.function()
+
+
+class ExtractDataTask(QObject):
+
+    extractDataFinishedSignal: pyqtBoundSignal = pyqtSignal(str)
+
+    def __init__(self, list_of_numbers):
+        super().__init__()
+        self.nums = list_of_numbers
+        self.powers_of_nums = []
+
+    def start_extraction(self):
+        self.powers_of_nums = [num ** 2 for num in self.nums]
+        self.extractDataFinishedSignal.emit("Extraction finished")
+
+
